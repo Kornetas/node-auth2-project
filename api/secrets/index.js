db7d8dc28579746198b55b1c81015a1802a9e13f
Arkadiusz Rak <arek93r@gmail.com>
2025-01-26 22:06:42 +0100

    wipo

--- api/secrets/index.js
+++ api/secrets/index.js
@@ -7,5 +7,6 @@
   developers cloning this repo won't be able to run the project as is.
  */
 module.exports = {
+  NODE_ENV: process.env.NODE_ENV || "development",
   JWT_SECRET: process.env.JWT_SECRET || "shh",
 };
